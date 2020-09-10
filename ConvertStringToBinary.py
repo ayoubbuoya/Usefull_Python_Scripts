6544d45ab3daaa85dcf0fad20e6093bc47b86b5f
ayoubbuoya <69482481+ayoubbuoya@users.noreply.github.com>
2020-09-10 12:46:03 +0200

    Update ConvertStringToBinary.py

--- ConvertStringToBinary.py
+++ ConvertStringToBinary.py
@@ -3,4 +3,4 @@ def convert_str_to_bin(string):
         binary="".join(format(ord(ch),"b") for ch in string)
         print("Your String Is :\n{}".format(string)) 
         print("Binary Of {} Is :\n{}".format(string,binary))
-convert_str_to_bin("ayoub"
+
